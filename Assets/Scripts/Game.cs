421a83b3cd2b3d2dd6bf48df72fb7169d3eb9888
enricovictorio <enricovictorio@gmail.com>
2024-09-14 10:47:42 +0800

    add function to play 2 pairs.

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -31,7 +31,7 @@ public class Game : GameBase
 
     public override bool OnConfirmMove(List<Card> pCardsToPlay)
     {
-        if (pCardsToPlay.Count > 1)
+        if (pCardsToPlay.Count > 2)
         {
             return false;
         }
@@ -77,7 +77,44 @@ public class Game : GameBase
 
     private bool Validate2CardPlay(List<Card> pCardsToPlay)
     {
-        return true;
+        int cardOnHandRank = pCardsToPlay[0].cardRankIndex;
+        if (pCardsToPlay[1].cardRankIndex != cardOnHandRank)
+        {
+            return false;
+        }
+
+        int cardOnTableRank = Game.Instance.Table.cardsInTable.Count < 1 ? -1 : Game.Instance.Table.cardsInTable[0].cardRankIndex;
+        if (cardOnHandRank > cardOnTableRank)
+        {
+            return true;
+        }
+        else if (cardOnHandRank == cardOnTableRank)
+        {
+            int cardOnTableHighestSuit = 0;
+            foreach (Card card in Game.Instance.Table.cardsInTable)
+            {
+                if (card.cardSuitIndex > cardOnTableHighestSuit)
+                {
+                    cardOnTableHighestSuit = card.cardSuitIndex;
+                }
+            }
+
+            int cardOnHandHighestSuit = 0;
+            foreach (Card card in pCardsToPlay)
+            {
+                if (card.cardSuitIndex > cardOnHandHighestSuit)
+                {
+                    cardOnHandHighestSuit = card.cardSuitIndex;
+                }
+            }
+
+            if (cardOnHandHighestSuit > cardOnTableHighestSuit)
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     private bool Validate3CardPlay(List<Card> pCardsToPlay)
